1f017c17699da57c0dc106de078089bbbfdeef64
Steven Haid <stevenhaid@gmail.com>
2021-04-13 09:38:36 -0400

    updated config_hw

--- robot/config_hw.h
+++ robot/config_hw.h
@@ -5,20 +5,28 @@
 extern "C" {
 #endif
 
-#define RELAY_TEST                   26                   // GPIO OUT
-#define RELAY_GPIO_PINS              ((1<<RELAY_TEST))
+// motor ctlrs
+#define LEFT_MOTOR                   "/dev/ttyACM1"       // USB serial device
+#define RIGHT_MOTOR                  "/dev/ttyACM0"       // USB serial device
 
-#define ENCODER0_GPIO_A              16                   // GPIO IN
-#define ENCODER0_GPIO_B              17                   // GPIO IN
+// motor encoders
+#define ENCODER_GPIO_LEFT_A          22                   // GPIO IN
+#define ENCODER_GPIO_LEFT_B          23                   // GPIO IN
+#define ENCODER_GPIO_RIGHT_A         24                   // GPIO IN
+#define ENCODER_GPIO_RIGHT_B         25                   // GPIO IN
 
-#define PROXIMITY_FRONT_GPIO_SIG     5                    // GPIO IN
-#define PROXIMITY_FRONT_GPIO_ENABLE  6                    // GPIO OUT
+// proximity sensors
+#define PROXIMITY_FRONT_GPIO_ENABLE  26                   // GPIO OUT
+#define PROXIMITY_FRONT_GPIO_SIG     27                   // GPIO IN
+#define PROXIMITY_REAR_GPIO_ENABLE   16                   // GPIO OUT
+#define PROXIMITY_REAR_GPIO_SIG      17                   // GPIO IN
 
-#define PROXIMITY_REAR_GPIO_SIG      18                   // GPIO IN
-#define PROXIMITY_REAR_GPIO_ENABLE   19                   // GPIO OUT
+// buttons
+#define BUTTON_LEFT                  18                   // GPIO_IN
+#define BUTTON_RIGHT                 19                   // GPIO_IN
 
-#define CURRENT_ADC_CHAN             0                    // ADC
-
+// current sensor
+#define CURRENT_ADC_CHAN             6                    // ADC
 
 #ifdef __cplusplus
 }
