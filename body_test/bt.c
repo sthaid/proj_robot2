6677fa1c7b9b128ef39f5e7e5d7f15139d7929a3
Steven Haid <stevenhaid@gmail.com>
2021-05-22 08:58:21 -0400

    update body_test

--- body_test/bt.c
+++ body_test/bt.c
@@ -571,16 +571,22 @@ static int process_cmdline(void)
         send_msg(MSG_ID_MC_DEBUG_CTL, &x, sizeof(x));
     } else if (strcmp(cmd, "log_mark") == 0) {
         send_msg(MSG_ID_LOG_MARK, NULL, 0);
-    } else if ( (strcmp(cmd, "cal")   == 0 && (proc_id = 1))  ||
-                (strcmp(cmd, "mcal")  == 0 && (proc_id = 2))  ||
-                (strcmp(cmd, "fwd")   == 0 && (proc_id = 5))  ||
-                (strcmp(cmd, "rev")   == 0 && (proc_id = 6))  ||
-                (strcmp(cmd, "rot")   == 0 && (proc_id = 7))  ||
-                (strcmp(cmd, "hdg")   == 0 && (proc_id = 8))  ||
-                (strcmp(cmd, "rad")   == 0 && (proc_id = 9))  ||
-                (strcmp(cmd, "tst21") == 0 && (proc_id = 21)) ||
-                (strcmp(cmd, "tst22") == 0 && (proc_id = 22)) ||
-                (strcmp(cmd, "tst23") == 0 && (proc_id = 23)) 
+    } else if ( (strcmp(cmd, "scal") == 0 && (proc_id =   1))  ||
+                (strcmp(cmd, "mcal") == 0 && (proc_id =   2))  ||
+                (strcmp(cmd, "fwd")  == 0 && (proc_id =  11))  ||
+                (strcmp(cmd, "rev")  == 0 && (proc_id =  12))  ||
+                (strcmp(cmd, "rot")  == 0 && (proc_id =  13))  ||
+                (strcmp(cmd, "hdg")  == 0 && (proc_id =  14))  ||
+                (strcmp(cmd, "rad")  == 0 && (proc_id =  15))  ||
+                (strcmp(cmd, "tst1") == 0 && (proc_id = 101)) ||
+                (strcmp(cmd, "tst2") == 0 && (proc_id = 102)) ||
+                (strcmp(cmd, "tst3") == 0 && (proc_id = 103)) ||
+                (strcmp(cmd, "tst4") == 0 && (proc_id = 104)) ||
+                (strcmp(cmd, "tst5") == 0 && (proc_id = 105)) ||
+                (strcmp(cmd, "tst6") == 0 && (proc_id = 106)) ||
+                (strcmp(cmd, "tst7") == 0 && (proc_id = 107)) ||
+                (strcmp(cmd, "tst8") == 0 && (proc_id = 108)) ||
+                (strcmp(cmd, "tst9") == 0 && (proc_id = 109)) 
                         )
     {
         struct msg_drive_proc_s x = { proc_id, {arg[0], arg[1], arg[2], arg[3]} };
