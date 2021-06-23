876cf72f3d4d3cc0a4bbb24fe3e42e0a51af98a5
Steven Haid <stevenhaid@gmail.com>
2021-06-23 07:19:56 -0400

    remove file_utils.ch

--- brain/devel/portaudio/file_utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef __FILE_UTILS_H__
-#define __FILE_UTILS_H__
-
-int file_write(char *fn, int max_chan, int max_data, int sample_rate, float **chan_data);
-int file_read(char *fn, int *max_chan, int *max_data, int *sample_rate, float **chan_data);
-void file_read_free(float **chan_data);
-
-#endif
