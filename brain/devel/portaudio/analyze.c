15a8bfd0868545ba2f58d705d7b01ddc429d0be9
Steven Haid <stevenhaid@gmail.com>
2021-06-30 16:00:24 -0400

    continue

--- brain/devel/portaudio/analyze.c
+++ brain/devel/portaudio/analyze.c
@@ -1,3 +1,14 @@
+// xxx
+// - make a stanalone demo pgm and put in new repo  LATER
+// - support microphone option
+// - define for using cross microphone config
+// - use parabola fit to find the peek
+//   - and use atan to det direction
+// - adjustment knobs for tuning
+// - integrate with leds 
+// - test on rpi
+// - add Time option, and filter params options, so different filter params can be scripted
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdint.h>
@@ -41,22 +52,53 @@ static void *get_data_from_file_thread(void *cx);
 static void process_data(float *frame, double time_secs, void *cx);
 
 static char *stars(double v, double max_v, int max_stars, char *s);
+static void print_frame_rate(double time_now);
 
 // -----------------  MAIN  ------------------------------------------------
 
 int main(int argc, char **argv)
 {
-    pthread_t tid;
-    //char *file_name = "4mic_0.wav";  // xxx arg for this and mic
-    char *file_name = "4mic_270.wav";  // xxx arg for this and mic
+    char *file_name = "4mic_0.wav";  // xxx temp default
 
+    #define USAGE \
+    "usage: analyze [-f file_name.wav]"
+
+    // use line buffered stdout
     setlinebuf(stdout);
 
-    printf("STARTING ...\n");
+    // get options
+    // xxx add mic, and default to mic
+    while (true) {
+        int ch = getopt(argc, argv, "f:h");
+        if (ch == -1) {
+            break;
+        }
+        switch (ch) {
+        case 'f':
+            file_name = optarg;
+            if (strstr(file_name, ".wav") == NULL) {
+                printf("ERROR: file_name must have '.wav' extension\n");
+                return 1;
+            }
+            break;
+        case 'h':
+            printf("%s\n", USAGE);
+            return 0;
+            break;
+        default:
+            return 1;
+        };
+    }
+
+    // print startup msg
+    printf("file_name = %s\n", file_name);
+    // xxx print other stuff, like filter options
 
     // create get_data_from_file_thread, and
     // wait for it to complete initialization
     if (file_name) {
+        pthread_t tid;
+
         get_data_from_file_init_status = 1;
         pthread_create(&tid, NULL, get_data_from_file_thread, file_name);
         while (get_data_from_file_init_status == 1) {
@@ -69,8 +111,9 @@ int main(int argc, char **argv)
     }
 
     // pause forever
-    printf("\nRUNNING ...\n");
     while (true) {
+        // xxx put debug prints in here
+        // xxx maybe want debug controls too
         pause();
     }
 
@@ -153,18 +196,10 @@ static void *get_data_from_file_thread(void *cx)
 
 static void process_data(float *frame, double time_now, void *cx)
 {
-#if 1
     // window = 0.5 secs
-    #define MIN_START_AMP    1.0   
-    #define MIN_END_AMP      10.0
+    #define MIN_START_AMP    0.5
+    #define MIN_END_AMP      5.0 
     #define MAX_CHAN_DATA    (SAMPLE_RATE/2)
-#endif
-#if 0
-    // window = 0.1 secs
-    #define MIN_START_AMP    0.4
-    #define MIN_END_AMP      4.0
-    #define MAX_CHAN_DATA    (SAMPLE_RATE/10)
-#endif
 
     #define WINDOW_DURATION   ((double)MAX_CHAN_DATA / SAMPLE_RATE)
 
@@ -176,6 +211,7 @@ static void process_data(float *frame, double time_now, void *cx)
     static int      idx;
     static double   amp;
     static double   time_sound_start;
+    static double   filter_cx[MAX_CHAN][10];  // xxx move decls to where first used
 
     bool got_sound;
     int  i;
@@ -183,42 +219,19 @@ static void process_data(float *frame, double time_now, void *cx)
     // notes:
     // - time values used in this routine have units of seconds
 
-#if 0
     // print the frame rate once per sec
-    static uint64_t frame_count;
-    static double   time_last_frame_rate_print;
-    static uint64_t frame_count_last_print;
-    frame_count++;
-    if (time_now - time_last_frame_rate_print >= 1) {
-        printf("FRAME RATE = %d\n", (int)(frame_count-frame_count_last_print));
-        frame_count_last_print = frame_count;
-        time_last_frame_rate_print = time_now;
-    }
-#endif
+    print_frame_rate(time_now);
 
     // increment data idx
     idx = (idx + 1) % MAX_CHAN_DATA;
 
-    // xxx filter
-    static double filter_cx[MAX_CHAN][10];
-#if 0
-    for (int chan = 0; chan < MAX_CHAN; chan++) {
-        frame[chan] = low_pass_filter_ex(frame[chan], filter_cx[chan], 1, 0.80);
-    }
-#endif
-
     // copy the input frame data to the static 'data' arrray
+    // xxx comment about filtering
     for (int chan = 0; chan < MAX_CHAN; chan++) {
-        //DATA(chan,0) = frame[chan];
-        //DATA(chan,0) = low_pass_filter_ex(frame[chan], filter_cx[chan], 1, 0.95);
-        //DATA(chan,0) = high_pass_filter_ex(frame[chan], filter_cx[chan], 1, 0.95);
-        //DATA(chan,0) = high_pass_filter_ex(frame[chan], filter_cx[chan], 1, 0.80);
-        DATA(chan,0) = high_pass_filter_ex(frame[chan], filter_cx[chan], 1, 0.75);  // looks okay
+        DATA(chan,0) = high_pass_filter_ex(frame[chan], filter_cx[chan], 1, 0.75);
     }
 
-
-#if 0
-    // XXX temp
+#if 0 // XXX temp
     if (time_now > 1 && time_now < 3) {
         printf("%0.3f  -  %10.3f\n", time_now, amp);
     }
@@ -228,7 +241,7 @@ static void process_data(float *frame, double time_now, void *cx)
     static double corr02[41];
     static double corr13[41];
     const int max = MAX_CHAN_DATA-1;
-#if 0
+#if 0  // xxx use defines for which channels
     for (i = -20; i <= 20; i++) {
         corr02[i+20] += DATA(0,-20) * DATA(2,-(20+i))  -
                         DATA(0,-(max-20)) * DATA(2,-(max-20+i));
@@ -261,23 +274,12 @@ static void process_data(float *frame, double time_now, void *cx)
     }
     if (time_sound_start > 0 && time_now >= time_sound_start + WINDOW_DURATION) {
         got_sound = (amp > MIN_END_AMP);
-        if (!got_sound) {
+        if (got_sound == false) {
             printf("%0.3f - *** DISCARD *** SOUND amp=%0.3f  intvl=%0.3f ... %0.3f\n", 
                    time_now, amp, time_sound_start, time_now);
         }
         time_sound_start = 0;
     }
-
-#if 0
-    static int flag = 1;
-    if (flag) {
-        if (time_now >= 1) {
-            printf("TEST\n");
-            got_sound = true;
-            flag = false;
-        }
-    }
-#endif
 
     // if there is not sound data to analyze then return
     if (got_sound == false) {
@@ -286,7 +288,7 @@ static void process_data(float *frame, double time_now, void *cx)
 
     // xxx comment
     printf("%0.3f - ANALYZE SOUND amp=%0.3f  intvl=%0.3f ... %0.3f\n", 
-                   time_now, amp, time_sound_start, time_now);
+                   time_now, amp, time_now-WINDOW_DURATION, time_now);
 
     double max_corr02=0, max_corr13=0;
     for (i = -20; i <= 20; i++) {
@@ -301,14 +303,9 @@ static void process_data(float *frame, double time_now, void *cx)
                corr02[i+20], stars(corr02[i+20], max_corr02, 30, s1),
                corr13[i+20],  stars(corr13[i+20], max_corr13, 30, s2));
     }
-
-
-    //memset(corr02,0,sizeof(corr02));
-    //memset(corr13,0,sizeof(corr13));
-    //memset(filter_cx,0,sizeof(filter_cx));
-    // xxx continue here
 }
 
+// xxx move to debug section
 static char *stars(double v, double max_v, int max_stars, char *s)
 {
     if (v < 0) v = 0;
@@ -319,4 +316,21 @@ static char *stars(double v, double max_v, int max_stars, char *s)
     s[n] = '\0';
 
     return s;
+}
+
+static void print_frame_rate(double time_now)
+{
+    static uint64_t frame_count;
+    static double   time_last_frame_rate_print;
+    static uint64_t frame_count_last_print;
+
+    // only needed for unit test
+    return;
+
+    frame_count++;
+    if (time_now - time_last_frame_rate_print >= 1) {
+        printf("FRAME RATE = %d\n", (int)(frame_count-frame_count_last_print));
+        frame_count_last_print = frame_count;
+        time_last_frame_rate_print = time_now;
+    }
 }
