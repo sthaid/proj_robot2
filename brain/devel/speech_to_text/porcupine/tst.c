e83db08e60f51b39dd2743bd3d42ba4684bdc38d
Steven Haid <stevenhaid@gmail.com>
2021-07-10 08:20:41 -0400

    wake word test

--- brain/devel/speech_to_text/porcupine/tst.c
+++ brain/devel/speech_to_text/porcupine/tst.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <errno.h>
 #include <dlfcn.h>
+#include <pthread.h>
 
 #include <pv_porcupine.h>
 #include <pa_utils.h>
@@ -17,6 +19,8 @@
 #define MODEL_PATH  "../../repos/Porcupine/lib/common/porcupine_params.pv"
 
 #define MAX_KEYWORD_PATHS (sizeof(keyword_paths) / sizeof(keyword_paths[0]))
+
+#define MAX_PLAYBACK_DATA (3*SAMPLE_RATE)  // 3 seconds
 
 //
 // variables
@@ -36,73 +40,170 @@ static const char *keyword_paths[] = {
             };
 static float sensitivities[MAX_KEYWORD_PATHS];
 
-static short *sound_data;
+static pv_porcupine_t *porcupine;
 
-static int frame_length;
-    pv_porcupine_t *porcupine;
+static short *porcupine_sound_data;
+static int    porcupine_frame_length;
+
+static float  playback_data[MAX_PLAYBACK_DATA];
+static int    playback_cnt = -1;
 
 //
 // prototypes
 //
 
+static void *playback_thread(void *cx);
+static int recv_mic_data(const float *frame, void *cx);
 static void init_lib_syms(void);
-static int put_frame(const float *frame, void *cx);
 
-// -------------------------------------------------------------------------------
+// -----------------  MAIN  ------------------------------------------------------
 
 int main(int argc, char **argv)
 {
     pv_status_t     pvrc;
     int             rc;
     int             sample_rate;
+    pthread_t       tid;
 
+    // use line buffered stdout
     setlinebuf(stdout);
 
+    // open the porcupine library and get function addresses
     init_lib_syms();
 
+    // init sensitivities array, using same value for each of the keywords being monitored
     for (int i = 0; i < MAX_KEYWORD_PATHS; i++) {
         sensitivities[i] = 0.8;
     }
 
+    // initialize the porcupine wake word detection engine, to monitor for
+    // the keyword(s) contained in keyword_paths
     pvrc = pv_porcupine_init_func(MODEL_PATH, MAX_KEYWORD_PATHS, keyword_paths, sensitivities, &porcupine);
     if (pvrc != PV_STATUS_SUCCESS) {
         printf("ERROR: pv_porcupine_init, %s\n", pv_status_to_string_func(pvrc));
         return 1;
     }
 
-    // verify sample rate
+    // verify the sample rate required by porcupine matches the SAMPLE_RATE value
+    // that this program s built with
     sample_rate = pv_sample_rate_func();
     if (sample_rate != SAMPLE_RATE) {
         printf("ERROR: sample_rate=%d is not expected %d\n", sample_rate, SAMPLE_RATE);
         return 1;
     }
 
-    // get frame_length
-    // XXX bytes ?
-    frame_length = pv_porcupine_frame_length_func();
-    printf("frame_length = %d\n", frame_length);
-    sound_data = malloc(frame_length*2);  // xxx use calloc
-    memset(sound_data, 0, frame_length*2);
+    // get the porcupine frame length, this is the number of sound data values
+    // that the pv_porcupine_process routine expects to receive for each call
+    porcupine_frame_length = pv_porcupine_frame_length_func();
+    printf("porcupine_frame_length = %d\n", porcupine_frame_length);
+    porcupine_sound_data = calloc(porcupine_frame_length, sizeof(short));
 
+    // initialize portaudio utils; these utils provide a simplified interface 
+    // to the portaudio library
     rc = pa_init();
     if (rc < 0) {
         printf("ERROR: pa_init\n");
         return 1;
     }
 
-    rc =  pa_record2(DEFAULT_INPUT_DEVICE,
-                     1,            // max_chan
-                     SAMPLE_RATE,  // sample_rate  XXX 
-                     put_frame,    // callback
-                     NULL,         // put_frame cx
-                     0);           // discard_samples count
+    // create playback thread;
+    // this thread is used to play short sound segments from after keyword detection
+    pthread_create(&tid, NULL, playback_thread, NULL);
+
+    // call portaudio util to start acquiring mic data;
+    // the recv_mic_data callback is called repeatedly with the mic data;
+    // the pa_record2 blocks until recv_mic_data returns non-zero (blocks forever in this pgm)
+    rc =  pa_record2(DEFAULT_INPUT_DEVICE,  // xxx  use topp define
+                     1,              // max_chan
+                     SAMPLE_RATE,    // sample_rate
+                     recv_mic_data,  // callback
+                     NULL,           // cx passed to recv_mic_data
+                     0);             // discard_samples count
     if (rc < 0) {
         printf("ERROR: pa_record2\n");
         return 1;
     }
 
+    // done
     return 0;
 }
+
+// -----------------  PLAYBCK THREAD  --------------------------------
+
+static void *playback_thread(void *cx)
+{
+    int rc;
+
+    while (true) {
+        // if the playback buffer is full then
+        //   call pa_play to play the playback_data
+        //   reset playback_cnt to -1, indicating that we're done with this playback_data
+        // endif
+        if (playback_cnt == MAX_PLAYBACK_DATA) {
+            rc = pa_play(DEFAULT_OUTPUT_DEVICE, 1, MAX_PLAYBACK_DATA, SAMPLE_RATE, playback_data);
+            if (rc < 0) {
+                printf("ERROR: pa_play failed\n");
+                exit(1);
+            }
+
+            playback_cnt = -1;
+        }
+
+        // short sleep, 10 ms
+        usleep(10000);
+    }
+
+    return NULL;
+}
+
+// -----------------  RECV MIC DATA  ---------------------------------
+
+static int recv_mic_data(const float *frame, void *cx)
+{
+    int         keyword;
+    pv_status_t pvrc;
+
+    static int  psd_cnt;
+
+    // if a keyword has been detected then
+    //   Save 3 secs of playback_data.
+    //   This buffer will be played by the playback_thread once it is full.
+    // else
+    //   Save sound data in porcupine_sound_data array.
+    //   Once this buffer is full it is passed to pv_porcupine_process_func 
+    //    to check for wake word detection.
+    //   If a wake word is detected then playback_cnt is set to 0, so that
+    //    subsequent calls to recv_mic_data wiill save the playback_data.
+    // endif
+
+    if (playback_cnt >= 0) {
+        if (playback_cnt < MAX_PLAYBACK_DATA) {
+            playback_data[playback_cnt++] = frame[0];
+        }
+    } else {
+        porcupine_sound_data[psd_cnt++] = frame[0] * 32767;
+
+        if (psd_cnt == porcupine_frame_length) {
+            psd_cnt = 0;
+
+            keyword = -1;
+            pvrc = pv_porcupine_process_func(porcupine, porcupine_sound_data, &keyword);
+            if (pvrc != PV_STATUS_SUCCESS) {
+                printf("ERROR: pv_porcupine_process, %s\n", pv_status_to_string_func(pvrc));
+                exit(1);
+            }
+
+            if (keyword != -1) {
+                printf("detected keyword %d\n", keyword);
+                playback_cnt = 0;
+            }
+        }
+    }
+
+    return 0;
+}
+
+// -----------------  OBTAIN ACCESS TO PORCUPINE LIBRARY  ------------
 
 static void init_lib_syms(void)
 {
@@ -130,148 +231,3 @@ static void init_lib_syms(void)
     GET_LIB_SYM(pv_porcupine_process);
     GET_LIB_SYM(pv_porcupine_frame_length);
 }
-
-static int put_frame(const float *frame, void *cx)
-{
-    pv_status_t pvrc;
-    int keyword;
-
-    static int cnt;
-
-    sound_data[cnt++] = frame[0] * 32767;
-
-    if (cnt == frame_length) {
-        keyword = -1;
-
-        pvrc = pv_porcupine_process_func(porcupine, sound_data, &keyword);
-        if (pvrc != PV_STATUS_SUCCESS) {
-            printf("ERROR: pv_porcupine_process, %s\n", pv_status_to_string_func(pvrc));
-            exit(1);
-        }
-
-        cnt = 0;
-
-        if (keyword != -1) {
-            printf("detected keyword %d\n", keyword);
-        }
-    }
-
-    return 0;
-}
-
-#if 0
-    #define MAX 160
-    static int cnt;
-    static short buffer[MAX];
-
-    // collect 160 values, in 16 bit format, and write them to stdout
-    buffer[cnt++] = frame[0] * 32767;
-    if (cnt == MAX) {
-        if (write(1, buffer, sizeof(buffer)) < 0) {
-            printf("ERROR: write failed\n");
-            return -1;
-        }
-        cnt = 0;
-    }
-
-    // continue
-    return 0;
-#endif
-
-
-#if 0
-/*
-    Copyright 2018-2020 Picovoice Inc.
-
-    You may not use this file except in compliance with the license. A copy of the license is located in the "LICENSE"
-    file accompanying this source.
-
-    Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
-    an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
-    specific language governing permissions and limitations under the License.
-*/
-
-#include <alsa/asoundlib.h>
-#include <dlfcn.h>
-#include <signal.h>
-#include <stdio.h>
-
-#include "pv_porcupine.h"
-
-static volatile bool is_interrupted = false;
-
-void interrupt_handler(int _) {
-    (void) _;
-    is_interrupted = true;
-}
-
-int main(int argc, char *argv[]) {
-    if (argc != 6) {
-        fprintf(stderr, "usage : %s library_path model_path keyword_path sensitivity input_audio_device\n", argv[0]);
-        exit(1);
-    }
-
-    signal(SIGINT, interrupt_handler);
-
-    const char *library_path = argv[1];
-    const char *model_path = argv[2];
-    const char *keyword_path = argv[3];
-    const float sensitivity = (float) atof(argv[4]);
-    const char *input_audio_device = argv[5];
-
-    void *porcupine_library = dlopen(library_path, RTLD_NOW);
-    if (!porcupine_library) {
-        fprintf(stderr, "failed to open library.\n");
-        exit(1);
-    }
-
-    char *error = NULL;
-
-    pv_porcupine_t *porcupine = NULL;
-    pv_status_t status = pv_porcupine_init_func(model_path, 1, &keyword_path, &sensitivity, &porcupine);
-    if (status != PV_STATUS_SUCCESS) {
-        fprintf(stderr, "'pv_porcupine_init' failed with '%s'\n", pv_status_to_string_func(status));
-        exit(1);
-    }
-
-
-
-
-    const int32_t frame_length = pv_porcupine_frame_length_func();
-    printf("XXXX frame_len  %d\n", frame_length);
-
-    int16_t *pcm = malloc(frame_length * sizeof(int16_t));
-    if (!pcm) {
-        fprintf(stderr, "failed to allocate memory for audio buffer\n");
-        exit(1);
-    }
-
-    while (!is_interrupted) {
-        const int count = snd_pcm_readi(alsa_handle, pcm, frame_length);
-        if (count < 0) {
-            fprintf(stderr, "'snd_pcm_readi' failed with '%s'\n", snd_strerror(count));
-            exit(1);
-        } else if (count != frame_length) {
-            fprintf(stderr, "read %d frames instead of %d\n", count, frame_length);
-            exit(1);
-        }
-
-        int32_t keyword_index = -1;
-        status = pv_porcupine_process_func(porcupine, pcm, &keyword_index);
-        if (status != PV_STATUS_SUCCESS) {
-            fprintf(stderr, "'pv_porcupine_process' failed with '%s'\n", pv_status_to_string_func(status));
-            exit(1);
-        }
-        if (keyword_index != -1) {
-            fprintf(stdout, "detected keyword\n");
-        }
-    }
-
-    free(pcm);
-    snd_pcm_close(alsa_handle);
-    pv_porcupine_delete_func(porcupine);
-    dlclose(porcupine_library);
-
-    return 0;
-}
-#endif
