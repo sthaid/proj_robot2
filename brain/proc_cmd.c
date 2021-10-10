bbe2818a3c328910e02a03f086466d449b053fbb
Steven Haid <stevenhaid@gmail.com>
2021-10-10 10:38:18 -0400

    add volume handler

--- brain/proc_cmd.c
+++ brain/proc_cmd.c
@@ -1,19 +1,31 @@
 #include <common.h>
 
+//
 // variables
+//
+
 static char *cmd;
 static bool cancel;
 
+//
 // prototypes
-static void *cmd_thread(void *cx);
+//
 
+static void *cmd_thread(void *cx);
+static int getnum(char *s, int default_value);
+static bool strmatch(char *s, ...);  // xxx utils
+
+//
 // handlers
-static int rotate_hndlr(args_t args);
-static int test_hndlr(args_t args);
+//
+
+static int hndlr_volume(args_t args);
+static int hndlr_sleep(args_t args);
 
 static hndlr_lookup_t hndlr_lookup_tbl[] = {
-    { "rotate", rotate_hndlr },
-    { "test",   test_hndlr },
+    { "volume", hndlr_volume },
+    { "sleep", hndlr_sleep },
+    { NULL, NULL }
                 };
 
 // -----------------  INIT  -------------------------------------------------
@@ -28,7 +40,6 @@ void proc_cmd_init(void)
 
 // -----------------  RUNTIME API  ------------------------------------------
 
-// xxx doa is tbd
 void proc_cmd_execute(char *transcript, double doa)
 {
     cmd = transcript;
@@ -62,6 +73,8 @@ static void *cmd_thread(void *cx)
         INFO("match=%d, args=  '%s'  '%s'  '%s'\n", match, args[0], args[1], args[2]);
         if (match) {
             proc(args);
+        } else {
+            t2s_play("Don't grok: %s.", cmd);   // xxx rename to transcript
         }
 
         // done with this cmd
@@ -73,24 +86,74 @@ static void *cmd_thread(void *cx)
     return NULL;
 }
 
-// -----------------  CMD HANDLERS  -----------------------------------------
+// -----------------  HNDLR - VOLUME  ---------------------------------------
 
-static int rotate_hndlr(args_t args)
+static int hndlr_volume(args_t args)
 {
-    INFO("rotate: proc=%s amount=%s dir=%s\n", args[0], args[1], args[2]);
-    for (int i=0; i < 10; i++) {
-        INFO("args[%d] = %s\n", i, args[i]);
+    char *action = args[0];
+    char *amount = args[1];
+
+    if (strmatch(action, "get", "query", "what", NULL)) {
+        t2s_play("The volume is %d%%.", t2s_get_volume());
+        return 0;  // xxx how is this ret used
+    }
+
+    if (strmatch(action, "up", "increase", NULL)) {
+        t2s_set_volume(getnum(amount, DELTA_VOLUME), true);
+    } else if (strmatch(action, "down", "decrease", NULL)) {
+        t2s_set_volume(-getnum(amount, DELTA_VOLUME), true);
+    } else if (strmatch(action, "set", NULL)) {
+        t2s_set_volume(getnum(amount, DEFAULT_VOLUME), false);
+    } else if (strmatch(action, "reset", NULL)) {
+        t2s_set_volume(DEFAULT_VOLUME, false);
+    }
+    t2s_play("The volume is now %d%%.", t2s_get_volume());
+
+    return 0;
+}
+
+// -----------------  HNDLR - SLEEP  ----------------------------------------
+
+static int hndlr_sleep(args_t args)
+{
+    int secs = getnum(args[0], 10);
+    
+    t2s_play("Sleeping for %d seconds.", secs);
+
+    for (int i = 0; i < secs; i++) {
+        sleep(1);
+        if (cancel) {
+            t2s_play("sleep has been cancelled");
+            break;
+        }
     }
 
     return 0;
 }
 
-static int test_hndlr(args_t args)
-{
-    INFO("test: proc=%s word=%s\n", args[0], args[1]);
-    for (int i=0; i < 10; i++) {
-        INFO("args[%d] = %s\n", i, args[i]);
-    }
+// -----------------  SUPPORT  ----------------------------------------------
 
-    return 0;
+static int getnum(char *s, int default_value)
+{
+    int n = default_value;
+    sscanf(s, "%d", &n);
+    return n;
+}
+
+static bool strmatch(char *s, ...)
+{
+    va_list ap;
+    bool match = false;
+    char *s1;
+
+    va_start(ap, s);
+    while ((s1 = va_arg(ap, char*))) {
+        if (strcmp(s, s1) == 0) {
+            match = true;
+            break;
+        }
+    }
+    va_end(ap);
+
+    return match;
 }
