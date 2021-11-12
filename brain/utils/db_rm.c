0f934cdd16c403b47955f028aeb291c5599fd071
Steven Haid <stevenhaid@gmail.com>
2021-11-12 09:07:03 -0500

    add db_rm

--- brain/utils/db_rm.c
+++ brain/utils/db_rm.c
@@ -1,11 +1,14 @@
 #include <utils.h>
 
-char *file_name = "db.dat";
+static char *file_name = "db.dat";
 
 static void usage(void);
 
 int main(int argc, char **argv)
 {
+    int rc, keyid;
+    char *keyidstr, *keystr;
+
     // init logging
     log_init(NULL, false, true);
     misc_init();
@@ -29,20 +32,33 @@ int main(int argc, char **argv)
         }
     }
 
+    // get the keyidstr and keystr
+    keyidstr = argv[optind];
+    keystr = argv[optind+1];
+
+    // verify
+    if (keyidstr == NULL || keystr == NULL || sscanf(keyidstr, "%d", &keyid) != 1) {
+        usage();
+        return 1;
+    }
+
     // open database
-    INFO("OPENING %s\n\n", file_name);
+    INFO("OPENING %s\n", file_name);
     db_init(file_name, false, 0);
+    
+    // remove the requested entry
+    INFO("REMOVING %d '%s'\n", keyid, keystr);
+    rc = db_rm(keyid, keystr);
+    if (rc != 0) {
+        ERROR("FAILED TO REMOVE %d %s\n", keyid, keystr);
+        return 1;
+    }
 
-    // dump database
-    INFO("DUMPING ...\n\n");
-    db_dump();
-
-    // done
+    // success
     return 0;
 }
 
 static void usage(void)
 {
-    ERROR("usage: db_dump [-f db_file]\n");
+    ERROR("usage: db_rm [-f db_file] keyid keystr\n");
 }
-
